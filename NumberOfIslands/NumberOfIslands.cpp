1157b686ba895425b508b2a48afc551b86a5a721
Eric Chien <EricChien@wits.com>
2026-01-21 21:48:54 +0800

    Update naming for NumberOfIsland

--- NumberOfIslands/NumberOfIslands.cpp
+++ NumberOfIslands/NumberOfIslands.cpp
@@ -3,8 +3,8 @@
 #include <string>
 #include <queue>
 //#include "UnionFind.h"
-//#include "DFS.h"
-#include "BFS.h"
+#include "DFS.h"
+//#include "BFS.h"
 
 using namespace std;
 
@@ -16,12 +16,18 @@ int main()
   //  { '1', '1', '0', '0', '0' },
   //  { '0', '0', '0', '0', '0' }
   //};
+  //vector<vector<char>> grid = {
+  //  { '1','1','0','0','0' },
+  //  { '1','1','0','0','0' },
+  //  { '0','0','1','0','0' },
+  //  { '0','0','0','1','1' }
+  //};
   vector<vector<char>> grid = {
-    { '1','1','0','0','0' },
-    { '1','1','0','0','0' },
-    { '0','0','1','0','0' },
-    { '0','0','0','1','1' }
+  { '1','1','1' },
+  { '0','1','0' },
+  { '1','1','1' }
   };
+
   Solution sol;
   int result = sol.numIslands(grid);
   cout << result << endl;
